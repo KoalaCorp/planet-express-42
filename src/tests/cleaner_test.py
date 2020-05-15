e2d57b2bc28a7f40a66c97b3aa33989d396558ff
ewokcillo <azotillo@gmail.com>
2020-05-15 19:57:58 +0200

    Add entities y tokenized section

--- src/tests/cleaner_test.py
+++ src/tests/cleaner_test.py
@@ -10,6 +10,10 @@ class TokenizedCleanTestTestCase(unittest.TestCase):
             json_data = json.load(json_file)
             json_data_tokenized = json_data['tokenized'][0]
             tokenized = Tokenized(json_data['content'])
-            self.assertEqual(tokenized.clean_text(), {
-                'topics': json_data_tokenized['topics'],
-                'names': json_data_tokenized['names']})
+            tokenized_clean_text = tokenized.clean_text()
+            self.assertEqual(tokenized_clean_text['topics'],
+                             json_data_tokenized['topics'])
+            self.assertEqual(tokenized_clean_text['names'],
+                             json_data_tokenized['names'])
+            self.assertEqual(tokenized_clean_text['entities'],
+                             json_data_tokenized['entities'])
